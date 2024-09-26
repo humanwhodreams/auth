d5a519a334e1c998cb143929681488ac90fd7baf
enoabasi <watermelonbit@gmail.com>
2024-09-27 00:56:30 +0100

    style: add tertiary styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,6 +37,10 @@ const config: Config = {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
         },
+        tertiary: {
+          DEFAULT: 'hsl(var(--tertiary))',
+          foreground: 'hsl(var(--tertiary-foreground))',
+        },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
           foreground: 'hsl(var(--muted-foreground))',
