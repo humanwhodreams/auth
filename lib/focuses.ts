17862adaf72220e3e68858da54e22f57aac03532
enoabasi <watermelonbit@gmail.com>
2024-09-27 00:18:39 +0100

    refac: add roundness to all focus ring
    - the `cn` function will take care of tailwind clashes

--- lib/focuses.ts
+++ lib/focuses.ts
@@ -1,5 +1,5 @@
 export const focusRing =
-  'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2';
+  'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-md';
 export const focusInput =
   'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0';
 export const focusError = 'focus-visible:ring-destructive';
