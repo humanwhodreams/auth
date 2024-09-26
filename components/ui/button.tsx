12bed811a201dffa04e618172c4a62382f4ebcf4
enoabasi <watermelonbit@gmail.com>
2024-09-27 00:57:14 +0100

    feat/style: add tertiary variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        tertiary:
+          'bg-tertiary text-tertiary-foreground hover:bg-tertiary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-muted-foreground underline-offset-4 hover:underline hover:text-primary',
         linkPrimary: 'text-primary underline-offset-4 hover:underline',
