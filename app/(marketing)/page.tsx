86ca315191f19b7387b3e42f2ebdc8ff3cfff473
enoabasi <watermelonbit@gmail.com>
2024-09-27 00:58:24 +0100

    refac: redesign landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,32 @@
-import { Button } from '@/components/ui/button';
-import { ThemeToggle } from '@/components/theme-toggle';
+import Link from 'next/link';
+import { Section } from '@/components/ui/section';
+import { buttonVariants } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="p-24 space-y-4">
-      <h1>Welcome to Auth</h1>
-      <div className="flex items-center gap-4">
-        <Button type="button">
-          <code>/Sign in</code>
-        </Button>
-        <ThemeToggle />
-      </div>
-    </div>
+    <>
+      <Section className="space-y-4">
+        <h1>Authentication workflow guide.</h1>
+        <p className="md:w-3/5 lead">
+          Full authentication flow built using Auth.js, React.js, Next.js,
+          Tailwindcss, Typescript, Drizzle, and NeonDB.
+        </p>
+
+        <div className='flex flex-col items-center gap-2 md:flex-row'>
+          <Link
+            href={'#'}
+            className={buttonVariants({ variant: 'tertiary', size: 'lg' })}
+          >
+            Get access &mdash; For Free
+          </Link>
+          <Link
+            href={'#'}
+            className={buttonVariants({ variant: 'secondary', size: 'lg' })}
+          >
+            GitHub
+          </Link>
+        </div>
+      </Section>
+    </>
   );
 }
