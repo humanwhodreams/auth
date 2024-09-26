2ec2502e71a470515c9d91f8c558ec4094964622
enoabasi <watermelonbit@gmail.com>
2024-09-27 00:57:33 +0100

    refac: change variant to ghost from outline

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -19,7 +19,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="iconSm">
+        <Button variant="ghost" size="iconSm">
           <SunDimIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 fill-current" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 fill-current" />
           <span className="sr-only">Toggle theme</span>
